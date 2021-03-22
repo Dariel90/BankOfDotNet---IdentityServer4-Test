b6dd9cacb5d66c8b83c4951dfca79eb032bb8372
Dariel <dariel901231@gmail.com>
2021-03-22 14:57:33 -0400

    Updating IS4 for use parsistance in Sql DB

--- BankOfDotNet.IdentitySvr/Startup.cs
+++ BankOfDotNet.IdentitySvr/Startup.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using IdentityServer4.Models;
-using IdentityServer4.Test;
+using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using IdentityServer4.EntityFramework.Mappers;
 
 namespace BankOfDotNet.IdentitySvr
 {
@@ -19,13 +19,31 @@ namespace BankOfDotNet.IdentitySvr
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(opts => opts.EnableEndpointRouting = false );
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json", false)
+                .Build();
+
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            var migrationAssembly = typeof(Startup).GetType().Assembly.GetName().Name;
+
             services.AddIdentityServer()
-                .AddInMemoryClients(Config.GetClients())
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetAllApiResources())
-                .AddInMemoryApiScopes(Config.GetApiScopes())
+                //.AddInMemoryClients(Config.GetClients())
+                //.AddInMemoryIdentityResources(Config.GetIdentityResources())
+                //.AddInMemoryApiResources(Config.GetAllApiResources())
+                //.AddInMemoryApiScopes(Config.GetApiScopes())
                 .AddTestUsers(Config.GetUsers())
-                .AddDeveloperSigningCredential();
+                .AddDeveloperSigningCredential()
+                .AddConfigurationStore(opts =>
+                {
+                    opts.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly("BankOfDotNet.IdentitySvr"));
+                })
+                //Operationl Store: Token, Consents, codes, etc
+                .AddOperationalStore(opts =>
+                {
+                    opts.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly("BankOfDotNet.IdentitySvr"));
+                });
 
             //Cookie policy stuff
             services.Configure<CookiePolicyOptions>(options =>
@@ -42,6 +60,8 @@ namespace BankOfDotNet.IdentitySvr
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            InitialIndentityServerDatabase(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -56,6 +76,45 @@ namespace BankOfDotNet.IdentitySvr
             app.UseAuthorization();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitialIndentityServerDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+
+                //Seed the data
+                //Seed Clients
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+                //Seed the identity resources
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+                //Seed the api reosurces
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetAllApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
 
         //Se activa la politica de Cookies a (1) pues en el caso de Chrome no redirije
